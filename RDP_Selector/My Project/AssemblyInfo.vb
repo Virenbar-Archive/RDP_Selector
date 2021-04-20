18878371969f0f0527ed7699c649ce1dee60001e
Virenbar <rib.artem@gmail.com>
2021-04-20 17:41:43 +0500

    Remove VS comments

--- RDP_Selector/My Project/AssemblyInfo.vb
+++ RDP_Selector/My Project/AssemblyInfo.vb
@@ -2,12 +2,6 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' Общие сведения об этой сборке предоставляются следующим набором
-' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
-' общие сведения об этой сборке.
-
-' Проверьте значения атрибутов сборки
-
 <Assembly: AssemblyTitle("RDP_Selector")>
 <Assembly: AssemblyDescription("Active Directory RDP Selector")>
 <Assembly: AssemblyCompany("Virenbar")>
@@ -16,20 +10,6 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
-
-'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 <Assembly: Guid("d705c3df-f478-4d96-984b-13c7630b3ba9")>
 
-' Сведения о версии сборки состоят из следующих четырех значений:
-'
-'      Основной номер версии
-'      Дополнительный номер версии
-'   Номер сборки
-'      Редакция
-'
-' Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
-' используя "*", как показано ниже:
-' <Assembly: AssemblyVersion("1.0.*")>
-
 <Assembly: AssemblyVersion("2.0.1")>
-<Assembly: AssemblyFileVersion("2.0.1")>
